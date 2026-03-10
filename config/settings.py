a1d933d147dac42493b6aac6c29b30887fece69d
Thiago Alves <thiagoalves.tav@gmail.com>
2026-03-10 13:35:03 -0300

    ajustando allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=2zb=$n%3-%#@56an_v%q9r+79swr@%yu3%$=ysf&mvf7wia$o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
